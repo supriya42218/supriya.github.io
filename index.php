8363acdb81a24ae4a0ea814612c6073c5d6fb82b
supriya42218 <32504389+supriya42218@users.noreply.github.com>
2017-10-04 10:37:45 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Home</title>
+</head>
+<body>
+<SCRIPT type="text/javascript" src="./fbapp/fb.js"> </SCRIPT>
+ <div id="content" style="width:70%; float;Left;">
+    <h2> WELCOME</h2>
+ </div>
+<div id= "sidebar" style="width:20%; float:right">
+	
+<div class="fb-login-button" data-scope ="public-profile,email" onlogin="checkLoginState();"></div>
+</div>
+</body>
+</html> 
